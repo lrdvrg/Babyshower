9bc258b524fb1ee8a3baf7c525b2cf7f4607ef34
Isai Vargas <isaidvargas@hotmail.com>
2019-11-15 11:54:56 -0400

    Yessirr

--- src/app/services/babyshower.service.ts
+++ src/app/services/babyshower.service.ts
@@ -24,7 +24,7 @@ export class BabyshowerService {
   constructor(private http: HttpClient, private toastr: ToastrService) { }
 
   getListado(){
-    this.http.get('http://babyshower.gear.host/api/Listados')
+    this.http.get('https://babyshower.gear.host/api/Listados')
     .subscribe(res=>{
         console.log(res);
         this.listado = res;
@@ -32,14 +32,14 @@ export class BabyshowerService {
   }
 
   removeListado(idlist: number){
-    this.http.delete('http://babyshower.gear.host/api/Listados/'+idlist)
+    this.http.delete('https://babyshower.gear.host/api/Listados/'+idlist)
     .subscribe(res=>{
         console.log(res);
       });
   }
 
   getUsuarios(){
-    this.http.get('http://babyshower.gear.host/api/Usuarios')
+    this.http.get('https://babyshower.gear.host/api/Usuarios')
     .subscribe(res=>{
         console.log(res);
         this.usuarios = res;
@@ -47,7 +47,7 @@ export class BabyshowerService {
   }
 
   getDeseosEscogidos(){
-    this.http.get('http://babyshower.gear.host/DeseosEscogidos')
+    this.http.get('https://babyshower.gear.host/api/DeseosEscogidos')
     .subscribe(res=>{
         console.log(res);
         this.escogidos = res;
@@ -56,20 +56,20 @@ export class BabyshowerService {
   }
 
   postDeseoEscogido(idlist: number){
-    this.http.post('http://babyshower.gear.host/DeseosEscogidos', this.seleccion)
+    this.http.post('https://babyshower.gear.host/api/DeseosEscogidos', this.seleccion)
     .subscribe(res=>{
       console.log(res);
 
       this.removeListado(idlist);
       this.getListado();
       this.getUsuarios();
-      this.toastr.success('Gracias','Accion completada');
+      this.toastr.success('Gracias','Tu eleccion fue registrada');
     });
 
   }
 
   getOriginal(){
-    this.http.get('http://babyshower.gear.host/api/Original')
+    this.http.get('https://babyshower.gear.host/api/Original')
     .subscribe(res=>{
         console.log(res);
         this.original = res;
